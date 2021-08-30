d98d7da714f90b9d8bc212fb18cc1fa748fd4c1e
AJG-Lumen <aaron.j.gomez1@lumen.com>
2021-08-30 07:03:43 +0000

    Final Project

--- connector.js
+++ connector.js
@@ -2,14 +2,6 @@ const request = require('request');
 
 const validResponseRegex = /(2\d\d)/;
 
-// Update this constant with your ServiceNow credentials
-const options = {
-  url: 'https://dev52920.service-now.com/',
-  username: 'admin',
-  password: 'Qt7NbS8rqHxS',
-  serviceNowTable: 'change_request'
-};
-
 /**
  * The ServiceNowConnector class.
  *
